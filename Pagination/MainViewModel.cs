8d097d8310707f9882be7449bb470295bab998e9
Kamilczak020 <kamilczak020@gmail.com>
2017-05-26 00:37:44 +0200

    Upload

---
+++ Pagination/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
+using PropertyChanged;
+using System.Windows.Input;
+
+namespace Pagination
+{
+    [ImplementPropertyChanged]
+    class MainViewModel : IViewModel
+    {
+        public ICommand ShowHomeViewCommand { get { return new RelayCommand(setHomeViewModel, () => true); } }
+        public ICommand ShowAuditViewCommand { get { return new RelayCommand(setAuditViewModel, () => true); } }
+
+        public IViewModel DisplayViewModel { get; set; }
+
+        public MainViewModel()
+        {
+            DisplayViewModel = new HomeViewModel();
+        }
+
+        private void setHomeViewModel()
+        {
+            DisplayViewModel = new HomeViewModel();
+
+        }
+
+        private void setAuditViewModel()
+        {
+            DisplayViewModel = new AuditViewModel();
+
+        }
+    }
+}
